d11d7aabd929cda9efabc5dd30eb26d992914525
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2024-08-12 02:38:25 +0530

    10.js

--- 10.js
+++ 10.js
@@ -1,9 +1,22 @@
-var result = db.catalog.aggregate([
-{ $match: { year: 2017 } },
-{ $group: { _id: "$genre", avgRating: { $avg: "$rating" } } },
-{ $sort: { avgRating: -1 } },
-{ $project: { _id: 0, genre: "$_id", avgRating: 1 } },
-{ $limit: 5 }
-]).toArray();
-print("Top 5 rated movie genres with their average rating:");
-printjson(result);
+
+result = db.catalog.aggregate([
+{$match:{year :2017}}, 
+{$group:{_id: "$genre", avgRating:{$avg: "$rating"}}}, 
+{$sort: {avgRating:-1}},
+{$project:{year:"$year", avgRating:1, genre:1} }, 
+{$limit:5} ]).toArray()
+print("Top 5 rated movie genres with their average rating")
+printjson(result)
+
+
+//Adding another stage using $skip
+
+result2 = db.catalog.aggregate([
+{$match:{year :2017}}, 
+{$group:{_id: "$genre", avgRating:{$avg: "$rating"}}}, 
+{$sort: {avgRating:-1}},
+{$project:{year:"$year", avgRating:1, genre:1} }, 
+{$limit:7},
+{$skip:2} ]).toArray()
+print("Top 7 rated movie genres with their average rating with he firsttwo skipped")
+printjson(result2)
